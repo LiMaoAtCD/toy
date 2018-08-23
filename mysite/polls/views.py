17be60435c15d994f517bf145e5fc2d5f0171074
AlienLi <liamscofield@icloud.com>
2018-08-23 08:48:02 +0800

    update

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import httpResponse
+
+def index(request):
+    return httpResponse("hello world you're at the polls index.")
